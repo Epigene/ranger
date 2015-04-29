ecb9b1804b988c10b1b2b03589eade22c8251819
Epigene <augusts.bautra@gmail.com>
2015-04-29 17:38:35 +0300

    Implemented Activerecord friendly created_at query from params

--- lib/ranger.rb
+++ lib/ranger.rb
@@ -1,4 +1,5 @@
 require "ranger/version"
+require 'active_support/all'
 
 class String
   def to_range
@@ -7,6 +8,12 @@ class String
         return rangeify("...")
       else
         return rangeify("..")
+      end
+    elsif self.match(/\A(\d{1,2}-\d{1,2}-\d{4})...?(\d{1,2}-\d{1,2}-\d{4})\z/)
+      if self.include?("...")
+        return d_rangeify("...")
+      else
+        return d_rangeify("..")
       end
     else
       raise ArgumentError, "Given string does not match /\A\d+...?\d+\z/"
@@ -17,5 +24,9 @@ class String
     def rangeify type # rangeify("..")
       two_range_integers = self.split(type)
       return eval(%Q|#{two_range_integers[0]}#{type}#{two_range_integers[1]}|)
+    end
+    def d_rangeify type
+      two_range_dates = self.split(type)
+      return two_range_dates[0].to_datetime.beginning_of_day..two_range_dates[1].to_datetime.end_of_day
     end
 end
--- spec/ranger_spec.rb
+++ spec/ranger_spec.rb
@@ -10,19 +10,33 @@ describe Ranger do
   end
 
   describe "#to_range" do
-    it 'raises ArgumentError if non-range-y string is given' do
-      expect{ "x..y".to_range }.to raise_error ArgumentError
+    context "integer..integer case" do
+      it 'raises ArgumentError if non-range-y string is given' do
+        expect{ "x..y".to_range }.to raise_error ArgumentError
+      end
+      it 'converts "1..3" to a range object' do
+        expect("1..3".to_range.class).to eq Range
+        expect("1..3".to_range).to eq 1..3
+      end
+      it 'converts "1...3" to a range object' do
+        expect("1...3".to_range.class).to eq Range
+        expect("1...3".to_range).to eq 1...3
+      end
     end
-    it 'converts "1..3" to a range object' do
-      expect("1..3".to_range.class).to eq Range
-      expect("1..3".to_range).to eq 1..3
+    context "date..date case" do
+      it 'raises ArgumentError if non-range-y string is given' do
+        expect{ "123-13-3112..sas-d-asd-t".to_range }.to raise_error ArgumentError
+      end
+      it 'converts "01-01-2015..02-02-2015" to a range object' do
+        expect("01-01-2015..02-02-2015".to_range).to eq ("Thu, 01 Jan 2015 00:00:00 +0000".to_datetime .. "Mon, 02 Feb 2015 23:59:59 +0000".to_datetime)
+      end
+      it 'converts "01-01-2015...02-02-2015" to a range object' do
+        expect("01-01-2015...02-02-2015".to_range).to eq ("Thu, 01 Jan 2015 00:00:00 +0000".to_datetime .. "Mon, 02 Feb 2015 23:59:59 +0000".to_datetime)
+      end
     end
-    it 'converts "1...3" to a range object' do
-      expect("1...3".to_range.class).to eq Range
-      expect("1...3".to_range).to eq 1...3
-    end
-
   end
+
+
 
 
 end
